a57ed9fd802d43c91436a94b418681e8f987b894
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-06 16:33:07 -0500

    📦 NEW: Add scaffolding

---
+++ src/graphql/queries.ts
@@ -0,0 +1,20 @@
+export const GET_FILMS = /* GraphQL */ `
+  query Query {
+    allFilms {
+      films {
+        title
+        director
+        releaseDate
+        speciesConnection {
+          species {
+            name
+            classification
+            homeworld {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
