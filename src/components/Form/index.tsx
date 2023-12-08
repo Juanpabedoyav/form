3c54ebbb8b06551cc9ed8c9fa58b110e9f5b43b3
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-07 23:59:39 -0500

    🐛 FIX: Fix import and use ApolloClient for API calls

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,26 +1,18 @@
-// import { Formik } from "formik"
+import { Formik } from "formik"
 // import { FormData } from "../../interfaces/formData"
-import { useEffect, useState } from "react"
 import {GET_FILMS} from "../../graphql/queries"
+import { useQuery, gql } from "@apollo/client"
 
+const query = gql(GET_FILMS)
 // const initialValues : FormData= {
 //     firstName: '',
 //     lastName: '',
 //     favoriteMovie: ''
 // }
 export default  function Form  () {
-const [favoriteMovies, setFavoriteMovies] = useState([])
 
-
-useEffect(() => {
-fetch('https://swapi-graphql.netlify.app/.netlify/functions/index',{
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({query: GET_FILMS})
-}).then(res => res.json()).then(res =>setFavoriteMovies(res.data.allFilms.films))
-}, [])
-
-
+const {data, loading, error} = useQuery(query)
+if (error) return <p>Error :(</p>
 
 const onSubmit = (e: React.FormEvent<HTMLFormElement>)  => {
     e.preventDefault()
@@ -42,12 +34,13 @@ const onSubmit = (e: React.FormEvent<HTMLFormElement>)  => {
                 <input type="text" id="last-name" />
             </div>
         </div>
-
         <div className="form-field">
             <label htmlFor="favorite">Favorite Star Wars movies</label>
             <select>
                 {
-                favoriteMovies.map((movie: { title: string }, i: number) => (
+                loading ? <option>Loading...</option> 
+                :
+                data.allFilms.films.map((movie: { title: string }, i: number) => (
                     <option key={i} value={movie.title}>{movie.title}</option>
                 ))
             }
